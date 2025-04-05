623ab53e5e74956bc59d33af849469bbce5f15f6
wolfganghuse <wolfgang.huse@nutanix.com>
2025-04-05 15:14:19 +0200

    lint

--- config/image/config.go
+++ config/image/config.go
@@ -1,10 +1,10 @@
-package repository
+package image
 
 import "github.com/crossplane/upjet/pkg/config"
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
-    p.AddResourceConfigurator("nutanix_image", func(r *config.Resource) {
-        r.ShortGroup = "image"
-    })
+        p.AddResourceConfigurator("nutanix_image", func(r *config.Resource) {
+                r.ShortGroup = "image"
+        })
 }
