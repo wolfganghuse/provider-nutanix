ab0cd3aba0570608ee7aeef0cf9560e0e610fa11
wolfganghuse <wolfgang.huse@nutanix.com>
2025-04-05 15:16:55 +0200

    lint

--- config/external_name.go
+++ config/external_name.go
@@ -11,7 +11,7 @@ import "github.com/crossplane/upjet/pkg/config"
 var ExternalNameConfigs = map[string]config.ExternalName{
 	// Import requires using a randomly generated ID from provider: nl-2e21sda
 	// "null_resource": config.IdentifierFromProvider,
-	  // Name is a parameter and it is also used to import the resource.
+	// Name is a parameter and it is also used to import the resource.
 	"nutanix_image": config.NameAsIdentifier,
 }
 
--- internal/clients/nutanix.go
+++ internal/clients/nutanix.go
@@ -5,84 +5,84 @@ Copyright 2021 Upbound Inc.
 package clients
 
 import (
-        "context"
-        "encoding/json"
+	"context"
+	"encoding/json"
 
-        "github.com/crossplane/crossplane-runtime/pkg/resource"
-        "github.com/pkg/errors"
-        "k8s.io/apimachinery/pkg/types"
-        "sigs.k8s.io/controller-runtime/pkg/client"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
-        "github.com/crossplane/upjet/pkg/terraform"
+	"github.com/crossplane/upjet/pkg/terraform"
 
-        "github.com/wolfganghuse/provider-nutanix/apis/v1beta1"
+	"github.com/wolfganghuse/provider-nutanix/apis/v1beta1"
 )
 
 const (
-        // error messages
-        errNoProviderConfig     = "no providerConfigRef provided"
-        errGetProviderConfig    = "cannot get referenced ProviderConfig"
-        errTrackUsage           = "cannot track ProviderConfig usage"
-        errExtractCredentials   = "cannot extract credentials"
-        errUnmarshalCredentials = "cannot unmarshal nutanix credentials as JSON"
+	// error messages
+	errNoProviderConfig     = "no providerConfigRef provided"
+	errGetProviderConfig    = "cannot get referenced ProviderConfig"
+	errTrackUsage           = "cannot track ProviderConfig usage"
+	errExtractCredentials   = "cannot extract credentials"
+	errUnmarshalCredentials = "cannot unmarshal nutanix credentials as JSON"
 
-        keyUsername = "username"
-        keyPassword = "password"
-        keyEndpoint = "endpoint"
-        keyPort     = "port"
-        keyInsecure = "insecure"
+	keyUsername = "username"
+	keyPassword = "password"
+	keyEndpoint = "endpoint"
+	keyPort     = "port"
+	keyInsecure = "insecure"
 )
 
 // TerraformSetupBuilder builds Terraform a terraform.SetupFn function which
 // returns Terraform provider setup configuration
 func TerraformSetupBuilder(version, providerSource, providerVersion string) terraform.SetupFn {
-        return func(ctx context.Context, client client.Client, mg resource.Managed) (terraform.Setup, error) {
-                ps := terraform.Setup{
-                        Version: version,
-                        Requirement: terraform.ProviderRequirement{
-                                Source:  providerSource,
-                                Version: providerVersion,
-                        },
-                }
+	return func(ctx context.Context, client client.Client, mg resource.Managed) (terraform.Setup, error) {
+		ps := terraform.Setup{
+			Version: version,
+			Requirement: terraform.ProviderRequirement{
+				Source:  providerSource,
+				Version: providerVersion,
+			},
+		}
 
-                configRef := mg.GetProviderConfigReference()
-                if configRef == nil {
-                        return ps, errors.New(errNoProviderConfig)
-                }
-                pc := &v1beta1.ProviderConfig{}
-                if err := client.Get(ctx, types.NamespacedName{Name: configRef.Name}, pc); err != nil {
-                        return ps, errors.Wrap(err, errGetProviderConfig)
-                }
+		configRef := mg.GetProviderConfigReference()
+		if configRef == nil {
+			return ps, errors.New(errNoProviderConfig)
+		}
+		pc := &v1beta1.ProviderConfig{}
+		if err := client.Get(ctx, types.NamespacedName{Name: configRef.Name}, pc); err != nil {
+			return ps, errors.Wrap(err, errGetProviderConfig)
+		}
 
-                t := resource.NewProviderConfigUsageTracker(client, &v1beta1.ProviderConfigUsage{})
-                if err := t.Track(ctx, mg); err != nil {
-                        return ps, errors.Wrap(err, errTrackUsage)
-                }
+		t := resource.NewProviderConfigUsageTracker(client, &v1beta1.ProviderConfigUsage{})
+		if err := t.Track(ctx, mg); err != nil {
+			return ps, errors.Wrap(err, errTrackUsage)
+		}
 
-                data, err := resource.CommonCredentialExtractor(ctx, pc.Spec.Credentials.Source, client, pc.Spec.Credentials.CommonCredentialSelectors)
-                if err != nil {
-                        return ps, errors.Wrap(err, errExtractCredentials)
-                }
-                creds := map[string]string{}
-                if err := json.Unmarshal(data, &creds); err != nil {
-                        return ps, errors.Wrap(err, errUnmarshalCredentials)
-                }
-                ps.Configuration = map[string]any{}
-                if v, ok := creds[keyUsername]; ok {
-                        ps.Configuration[keyUsername] = v
-                }
-                if v, ok := creds[keyPassword]; ok {
-                        ps.Configuration[keyPassword] = v
-                }
-                if v, ok := creds[keyEndpoint]; ok {
-                        ps.Configuration[keyEndpoint] = v
-                }
-                if v, ok := creds[keyPort]; ok {
-                        ps.Configuration[keyPort] = v
-                }
-                if v, ok := creds[keyInsecure]; ok {
-                        ps.Configuration[keyInsecure] = v
-                }
-                return ps, nil
-        }
+		data, err := resource.CommonCredentialExtractor(ctx, pc.Spec.Credentials.Source, client, pc.Spec.Credentials.CommonCredentialSelectors)
+		if err != nil {
+			return ps, errors.Wrap(err, errExtractCredentials)
+		}
+		creds := map[string]string{}
+		if err := json.Unmarshal(data, &creds); err != nil {
+			return ps, errors.Wrap(err, errUnmarshalCredentials)
+		}
+		ps.Configuration = map[string]any{}
+		if v, ok := creds[keyUsername]; ok {
+			ps.Configuration[keyUsername] = v
+		}
+		if v, ok := creds[keyPassword]; ok {
+			ps.Configuration[keyPassword] = v
+		}
+		if v, ok := creds[keyEndpoint]; ok {
+			ps.Configuration[keyEndpoint] = v
+		}
+		if v, ok := creds[keyPort]; ok {
+			ps.Configuration[keyPort] = v
+		}
+		if v, ok := creds[keyInsecure]; ok {
+			ps.Configuration[keyInsecure] = v
+		}
+		return ps, nil
+	}
 }
